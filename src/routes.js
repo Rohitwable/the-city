c18e51fe4bf416ff160a5836954d82f1d5c1b6ce
Rohitwable <rohitwable46@gmail.com>
2019-05-04 15:35:48 +0530

    home featured animation

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from './hoc/layout'
+import { Switch, Route } from 'react-router-dom'
+import Home from './components/home'
 
 const Routes = () => (
     <Layout>
-        Hello
-       </Layout>
+        <Switch>
+            <Route exact path='/' component={Home} />
+        </Switch>
+    </Layout>
 )
 
 export default Routes
